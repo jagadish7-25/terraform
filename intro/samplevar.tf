f3a5b2951ce4404d1d667562ef0d94c850be2a15
“jagadish7-25” <,kadapajag9@gmail.com>
2023-11-17 11:40:23 +0100

    var

---
+++ intro/samplevar.tf
@@ -0,0 +1,19 @@
+variable "fruit_name" {
+  default = " apple"
+}
+
+output "fruit_name" {
+  value = "var.fruit_name"
+
+}
+
+
+variable "jagadish_k"{
+
+  default = " kadapa"
+
+}
+
+output "jagadish_k" {
+  value = " var.jagadish_k"
+}
