6dde52918ac8ec02729afff94a236a0a0fa02bb3
“jagadish7-25” <,kadapajag9@gmail.com>
2023-11-22 07:55:07 +0100

    modules.roboshop

--- roboshopv2/module/main.tf
+++ roboshopv2/module/main.tf
@@ -8,13 +8,3 @@ resource "aws_instance" "instance" {
   }
 }
 
-
-resource "aws_instance" "instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
-  tags = {
-    Name = var.name
-  }
-}
